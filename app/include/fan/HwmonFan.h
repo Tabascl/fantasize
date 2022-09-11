bbdd612209fb9eb4484dbe3626a30ed0f538927e
Simon <info@simonwil.de>
2022-09-11 20:06:08 +0200

    Integrate PwmControl a bit more, add stub for PwmSensor

--- app/include/fan/HwmonFan.h
+++ app/include/fan/HwmonFan.h
@@ -2,11 +2,16 @@
 #define HWMONFAN_H_
 
 #include <fan/Fan.h>
+#include <fan/PwmControl.h>
 
 class HwmonFan : public Fan {
 public:
   void PWM(int percent) override;
   int RPM() override;
+
+private:
+  PwmControl mPwmControl;
+  // PwmSensor mPwmSensor;
 };
 
 #endif // HWMONFAN_H_
